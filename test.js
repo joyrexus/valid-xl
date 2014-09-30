1922298b969dd1765577f13ff9fe39e9b66b29d4
joyrexus <joyrexus@gmail.com>
2014-09-29 21:38:58 -0500

    convert to tape tests

--- test.js
+++ test.js
@@ -1,58 +1,57 @@
-var assert = require('assert');
+var test = require('tape');
 var check = require('sample.constraints');
 
-var test_constraints = function() {
-    var test_LRB = function() {
-        assert(check.LRB('') === undefined, "permit empty strings");
-        assert(check.LRB('X') === 'LRB = `X` is an invalid value');
-        assert(check.LRB('L') === undefined);
-        assert(
-            check.LRB(' L') === 'LRB = ` L` is an invalid value',
+test('column constraints', function(t) {
+
+    test('LRB column constraints', function(t) {
+
+        t.equal(check.LRB(''), undefined, "permit empty strings");
+        t.equal(check.LRB('X'), 'LRB = `X` is an invalid value');
+        t.equal(check.LRB('L'), undefined);
+        t.equal(
+            check.LRB(' L'), 'LRB = ` L` is an invalid value',
             "check for spacing"
         );
-        assert(
-            check.LRB('LL') === 'LRB = `LL` is an invalid value',
+        t.equal(
+            check.LRB('LL'), 'LRB = `LL` is an invalid value',
             "require prefix operators"
         );
-        assert(check.LRB('L+L') === undefined, "permit multiple code chars");
-        assert(check.LRB('L+R') === undefined);
-        assert(check.LRB('L+R+B') === undefined);
-        assert(
-            check.LRB('L+X') === 'LRB = `L+X` is an invalid value',
+        t.equal(check.LRB('L+L'), undefined, "permit multiple code chars");
+        t.equal(check.LRB('L+R'), undefined);
+        t.equal(check.LRB('L+R+B'), undefined);
+        t.equal(
+            check.LRB('L+X'), 'LRB = `L+X` is an invalid value',
             "only permit valid code chars"
         );
-        assert(
-            check.LRB('L + R') === 'LRB = `L + R` is an invalid value',
+        t.equal(
+            check.LRB('L + R'), 'LRB = `L + R` is an invalid value',
             "do not permit inline spacing"
         );
-    };
 
-    var test_XYZ = function() {
-        assert(check.XYZ('') === undefined);
-        assert(check.XYZ('x') === undefined);
-        assert(check.XYZ('y') === undefined);
-        assert(check.XYZ('z') === undefined);
-        assert(check.XYZ('q') === 'XYZ = `q` is an invalid value');
-    };
+        t.end();
+    });
 
-    test_LRB();
-    test_XYZ();
-};
+    test('XYZ column constraints', function (t) {
 
-var test_validate = function() {
+        t.plan(5);
+        t.equal(check.XYZ(''), undefined, "permit empty strings");
+        t.equal(check.XYZ('x'), undefined);
+        t.equal(check.XYZ('y'), undefined);
+        t.equal(check.XYZ('z'), undefined);
+        t.equal(check.XYZ('q'), 'XYZ = `q` is an invalid value');
+    });
+
+    t.end();
+});
+
+
+test('the core module', function (t) {
+
+    t.plan(2);
     var validate = require('./');
     var file = 'sample.xlsx', 
-        sheet = 'Transcript'
+        sheet = 'Transcript';
     report = validate(file, sheet, check).report;
-    assert(report.invalid === 4);
-    assert(Object.keys(report.errors).length === 3, "3 lines have errors");
-}
-
-// run all tests here
-runTests = function() {
-    test_constraints();
-    test_validate();
-    console.log('all tests passed!');
-};
-
-runTests()
+    t.equal(report.invalid, 4, "total invalid");
+    t.equal(Object.keys(report.errors).length, 3, "3 lines have errors");
+});
